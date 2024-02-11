d2867b3651ec30e06c958eae566ba399ad04bc9f
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2024-02-11 10:47:20 +0530

    other basic routes setup using react router dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,10 @@
 import { useEffect } from "react";
 import { useLocation, BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./scenes/home/Home";
+import ItemDetails from "./scenes/itemDetails/ItemDetails";
+import Checkout from "./scenes/checkout/Checkout";
+import Confirmation from "./scenes/checkout/Confirmation";
+import Navbar from "./scenes/global/Navbar";
 
 function ScrollToTop() {
   const { pathname } = useLocation();
@@ -14,9 +18,13 @@ function App() {
 
   return (
     <BrowserRouter>
+      <Navbar />
       <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="item/:itemId" element={<ItemDetails />} />
+        <Route path="checkout" element={<Checkout />} />
+        <Route path="checkout/success" element={<Confirmation />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ client/src/scenes/itemDetails/ItemDetails.jsx
@@ -0,0 +1,7 @@
+export default function ItemDetails(){
+    return (
+        <div>
+            <h1>Welcome to the ItemDetails Page</h1>
+        </div>
+    );
+}
