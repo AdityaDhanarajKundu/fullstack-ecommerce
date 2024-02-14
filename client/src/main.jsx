efa81310a6880b5c4ee8bd04b2e3cf27c8eb31fe
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2024-02-14 09:54:27 +0530

    configuring our store and wrapping our application with redux toolkit provider to access the store

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import {ThemeProvider} from '@mui/material/styles'
-import {CssBaseline} from '@mui/material'
-import {theme} from './theme'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { ThemeProvider } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+import { theme } from "./theme";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import cartReducer from "./state/index.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// configuring the store
+const store = configureStore({
+  reducer: {
+    cart: cartReducer,
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </Provider>
+  </React.StrictMode>
+);
