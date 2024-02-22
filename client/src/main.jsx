3f1236055acd7eef9811026afe628c56b79e576d
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2024-02-22 13:40:31 +0530

    subscription page home and vite bug fixes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { ThemeProvider } from "@mui/material/styles";
-import { CssBaseline } from "@mui/material";
+import {CssBaseline} from "@mui/material";
 import { theme } from "./theme";
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
