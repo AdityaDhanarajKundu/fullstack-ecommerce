e98ee95a1cfdd74eebe3ba8f8723738ddd0ad9fc
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2024-02-24 01:35:57 +0530

    completed the payment component in the checkout page

--- client/src/scenes/checkout/Payment.jsx
+++ client/src/scenes/checkout/Payment.jsx
@@ -1,9 +1,50 @@
+import { Box, Typography } from "@mui/material";
+import TextField from "@mui/material/TextField";
+import PropTypes from "prop-types";
 
-
-const Payment = () => {
+const Payment = ({ values, touched, errors, handleBlur, handleChange }) => {
   return (
-    <div>Payment</div>
-  )
-}
+    <Box m="30px 0">
+      {/* CONTACT INFO */}
+      <Box>
+        <Typography sx={{ mb: "15px" }} fontSize="18px">
+          Contact Info
+        </Typography>
+        <TextField
+          fullWidth
+          type="text"
+          label="Email"
+          onBlur={handleBlur}
+          onChange={handleChange}
+          value={values.email}
+          name="email"
+          error={!!touched.email && !!errors.email}
+          helperText={touched.email && errors.email}
+          sx={{ gridColumn: "span 4", marginBottom: "15px" }}
+        />
+        <TextField
+          fullWidth
+          type="text"
+          label="Phone Number"
+          onBlur={handleBlur}
+          onChange={handleChange}
+          value={values.phoneNumber}
+          name="phoneNumber"
+          error={!!touched.phoneNumber && !!errors.phoneNumber}
+          helperText={touched.phoneNumber && errors.phoneNumber}
+          sx={{ gridColumn: "span 4" }}
+        />
+      </Box>
+    </Box>
+  );
+};
+
+Payment.propTypes = {
+  values: PropTypes.object,
+  touched: PropTypes.object,
+  errors: PropTypes.object,
+  handleBlur: PropTypes.func,
+  handleChange: PropTypes.func,
+};
 
 export default Payment;
