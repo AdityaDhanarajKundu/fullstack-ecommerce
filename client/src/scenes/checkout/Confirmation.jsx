3606889703483490cc80817fc4c6bc8662934cc7
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2024-02-24 10:10:12 +0530

    confirmation page

--- client/src/scenes/checkout/Confirmation.jsx
+++ client/src/scenes/checkout/Confirmation.jsx
@@ -1,7 +1,15 @@
-export default function Confirmation(){
-    return (
-        <div>
-            <h1>Welcome to the Confirmation Page</h1>
-        </div>
-    );
+import { Box } from "@mui/material";
+import Alert from "@mui/material/Alert";
+import AlertTitle from "@mui/material/AlertTitle";
+
+export default function Confirmation() {
+  return (
+    <Box m="90px auto" width="80%" height="50vh">
+      <Alert severity="success">
+        <AlertTitle>Success</AlertTitle>
+        You have successfully made an Order —{" "}
+        <strong>Congrats on Making your Purchase</strong>
+      </Alert>
+    </Box>
+  );
 }
